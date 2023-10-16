e9756f00f3e01c7c938b9a6a479418d0b2074b36
Kiash254 <codewithkiash@gmail.com>
2023-10-16 12:20:30 +0300

    added img to text

---
+++ img2txt.py
@@ -0,0 +1,14 @@
+from dotenv import find_dotenv,load_dotenv
+from transformers import pipeline
+load_dotenv(find_dotenv())
+
+#imagetotext
+
+def img2text(url):
+    image_to_text=pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+    
+    text=image_to_text(url)
+    
+    print(text)
+    return text
+    img2text("AKL_0483.jpg")
